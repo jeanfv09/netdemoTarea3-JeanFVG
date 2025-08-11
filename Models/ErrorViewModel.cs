5b2584e55e9a20aff88773ef8a747c6148ff2b57
jeanfv09 <jeanfrancov09@gmail.com>
2025-08-10 20:17:52 -0500

    Proyecto inicial MVC para veterinaria

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace netdemoTarea3.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
